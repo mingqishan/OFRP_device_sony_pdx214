7eefcf502a213e2dd421226058e3c7c6fa8b35a6
mingqishan <98464053+mingqishan@users.noreply.github.com>
2024-01-19 01:23:38 +0800

    Add files via upload

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,14 @@
-# Android device tree for Sony Pdx214 (pdx214)
-
-```
 #
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_pdx214.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_pdx214-user \
+    omni_pdx214-userdebug \
+    omni_pdx214-eng
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-# Android device tree for Sony Pdx214 (pdx214)
-
-```
 #
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+add_lunch_combo omni_pdx214-user
+add_lunch_combo omni_pdx214-userdebug
+add_lunch_combo omni_pdx214-eng
